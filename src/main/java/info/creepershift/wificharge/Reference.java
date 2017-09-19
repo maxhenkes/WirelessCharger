e1952d1bc09c2b211aabb12474c1b5ceee370865
CreeperShift <maxhenkes@gmail.com>
2017-09-19 03:36:49 +0200

    Updated Version to 1.2.0

--- src/main/java/info/creepershift/wificharge/Reference.java
+++ src/main/java/info/creepershift/wificharge/Reference.java
@@ -3,7 +3,7 @@ package info.creepershift.wificharge;
 public class Reference {
 
     public static final String MODID = "wificharge";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
     public static final boolean dev = false;
 	
 }
